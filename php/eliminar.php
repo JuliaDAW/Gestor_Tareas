e62626fb52de741437890591104e50d26b044bd1
Julia <julial.herand@educa.jcyl.es>
2024-01-31 20:57:57 +0100

    elimnar

--- php/eliminar.php
+++ php/eliminar.php
@@ -1,2 +1,9 @@
 <?php
+    require_once("conexion.php");
+
+    $conectar=conectar("notas");
+
+    $array=array(":id"=>$_POST["id"]);
+    $sql=$conectar->prepare("DELETE FROM notas WHERE id=:id");
+    $sql->execute();
 ?>
