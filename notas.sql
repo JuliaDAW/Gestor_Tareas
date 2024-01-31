cd554c2e8a5a62a2373cde69399ac41a1fe5ab5f
Julia <julial.herand@educa.jcyl.es>
2024-01-31 20:45:24 +0100

    conexion

--- notas.sql
+++ notas.sql
@@ -5,6 +5,6 @@ use notas;
 create table nota(
     id int auto_increment,
     nombre varchar(255) unique,
-    descripción varchar(255)
-    primary key(id);
+    descripción varchar(255),
+    primary key(id)
 )
