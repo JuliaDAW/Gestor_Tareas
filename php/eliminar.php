c40a9e531b4e6d7d95691aa717029787465255d0
Fernando <ferestga8@gmail.com>
2024-02-01 20:58:29 +0100

    Añadida la funcionalidad de eliminar varias

--- php/eliminar.php
+++ php/eliminar.php
@@ -2,7 +2,24 @@
     require_once("conexion.php");
 
     $conectar=conectar("notas");
-
+    if(!isset($_POST["id"])){
+        echo json_encode(array("status"=>"error"));
+        http_response_code(400);
+        exit;
+    }
+    $id = $_POST["id"];
+    if(is_array($id)){
+        $placeholders = implode(',', array_fill(0, count($id), '?'));
+        $sql=$conectar->prepare("DELETE FROM nota WHERE id IN ($placeholders)");
+        $sql->execute($id);
+        $rows=$sql->rowCount();
+        if($rows>0){
+            echo json_encode(array("status"=>"ok"));
+        }else{
+            echo json_encode(array("status"=>"error no existe ese ID"));
+        }
+        exit;
+    }
     $array=array(":id"=>$_POST["id"]);
     $sql=$conectar->prepare("DELETE FROM nota WHERE id=:id");
     $sql->execute($array);
