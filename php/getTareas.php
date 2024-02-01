0f8f70248a020321f3fe434d102530a0d471e215
Fernando <ferestga8@gmail.com>
2024-02-01 18:08:24 +0100

    Solucionado error

--- php/getTareas.php
+++ php/getTareas.php
@@ -3,18 +3,23 @@
     require_once('conexion.php');
     $conn = conectar('notas');
     $sql = "SELECT * FROM nota";
+    $whereConditions = [];
     if(isset($_POST["id"])) {
-        $sql = $sql . " WHERE id=:id";
+        $whereConditions[] = "id=:id";
     }
     if(isset($_POST["nombre"])) {
-        $sql = $sql . "WHERE nombre LIKE ':nombre'";
+        $whereConditions[] = "nombre LIKE :nombre";
+    }
+    if(!empty($whereConditions)) {
+        $sql .= " WHERE " . implode(' AND ', $whereConditions);
     }
     $stmt = $conn->prepare($sql);
     if(isset($_POST['id'])) {
         $stmt->bindParam(':id', $_POST['id']);
     }
     if(isset($_POST['nombre'])) {
-        $stmt->bindParam(':nombre', $_POST['nombre']);
+        $nombre = '%' . $_POST['nombre'] . '%'; // Almacenar el resultado en una variable
+        $stmt->bindParam(':nombre', $nombre); // Pasar la variable a bindParam()
     }
     $stmt->execute();
     $tareas = $stmt->fetchAll(PDO::FETCH_ASSOC);
