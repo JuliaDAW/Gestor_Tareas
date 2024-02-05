c02f5aa8c2125ab647061905bdb096a403b4e980
Julia <julial.herand@educa.jcyl.es>
2024-02-05 20:58:41 +0100

    sql modificado

--- notas.sql
+++ notas.sql
@@ -6,6 +6,7 @@ create table nota(
     id int auto_increment,
     nombre varchar(255) unique,
     descripción varchar(255),
+    hecho boolean default 0,
     primary key(id)
 );
-INSERT INTO nota VALUES (null, "Primera Nota", "Ejemplo de Descripción"), (null, "Segunda Nota", "Ejemplo de Descripción")
+INSERT INTO nota VALUES (null, "Primera Nota", "Ejemplo de Descripción", 0), (null, "Segunda Nota", "Ejemplo de Descripción", 0)
