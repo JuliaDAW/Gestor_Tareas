6ddfd4b1fbe6d5fb125ee2e591afd75982c83f8a
Julia <julial.herand@educa.jcyl.es>
2024-02-07 19:57:16 +0100

    hecho

--- js/index.js
+++ js/index.js
@@ -228,7 +228,11 @@ function datos_ajax(datos){ //coloca las notas en la tabla
     limpiar_tabla();
 
     $(datos).each(function(nota){
-        const b_hecho = this.hecho==1 ? "Hecho" : "No hecho";
+        const b_hecho = this.hecho==1 ? '<button class="si_hecho" style="margin: auto; cursor: default" type="button">'+
+            '<svg height="15px" viewBox="0 -1.5 11 11" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#ffffff" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>done_mini [#1484]</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-304.000000, -366.000000)" fill="#ffffff"> <g id="icons" transform="translate(56.000000, 160.000000)"> <polygon id="done_mini-[#1484]" points="259 207.6 252.2317 214 252.2306 213.999 252.2306 214 248 210 249.6918 208.4 252.2306 210.8 257.3082 206"> </polygon> </g> </g> </g> </g></svg>'+
+        '</button>' : '<button class="no_hecho" style="margin: auto; cursor: default" type="button">'+
+            '<svg height="15px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Menu / Close_MD"> <path id="Vector" d="M18 18L12 12M12 12L6 6M12 12L18 6M12 12L6 18" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg>'+
+        '</button>';
         $("#tabla_notas").append("<tr>"+
             "<td> <input type='checkbox' id_check='"+this.id+"'/> </td>"+
             "<td class='centrar_id'>"+this.id+"</td>"+
