d308f3c3a24f076316a4d363056f6a4c485f182d
Fernando <ferestga8@gmail.com>
2024-02-07 19:30:25 +0100

    Actualizado php

--- php/getTareas.php
+++ php/getTareas.php
@@ -10,12 +10,18 @@
     if(isset($_POST["nombre"])) {
         $whereConditions[] = "nombre LIKE :nombre";
     }
+    if(isset($_POST["hecho"])) {
+        $whereConditions[] = "hecho=:hecho";
+    }
     if(!empty($whereConditions)) {
         $sql .= " WHERE " . implode(' AND ', $whereConditions);
     }
     $stmt = $conn->prepare($sql);
     if(isset($_POST['id'])) {
         $stmt->bindParam(':id', $_POST['id']);
+    }
+    if(isset($_POST['hecho'])) {
+        $stmt->bindParam(':hecho', $_POST['hecho']);
     }
     if(isset($_POST['nombre'])) {
         $nombre = '%' . $_POST['nombre'] . '%'; // Almacenar el resultado en una variable
